025b93d92f2b139369eb531b75b6b6a010579dc3
JoelTipke <jtipke@hotmail.com>
2014-01-23 05:06:38 -0800

    Initial project check in.

---
+++ MVC4-SharePointVirtual/Controllers/OtherController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4_SharePointVirtual.Controllers
+{
+    public class OtherController : Controller
+    {
+        //
+        // GET: /Other/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
